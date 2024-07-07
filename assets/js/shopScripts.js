49f5946da5e666a06dd55e67487480c795b37618
AussieDev81 <aussiedev81@gmail.com>
2024-07-07 10:07:45 +1000

    Rearranged product card and product source info to allow extra images etc

--- assets/js/shopScripts.js
+++ assets/js/shopScripts.js
@@ -30,29 +30,22 @@ function renderProducts(sourceFile, productGroup) {
 
 			// Add each product card to the product grid
 			products.forEach((product) => {
-				// Card div
+				// Product card div
 				let productCard = document.createElement("div");
 				productCard.id = `${productGroup.Name}-product-${product.id}`;
 				productCard.className = "product-card";
 
-				// Add product image
-				let productFeatureImage = document.createElement("img");
-				productFeatureImage.src = product.imgSrc;
-				productFeatureImage.alt = product.imgAlt;
-				productCard.appendChild(productFeatureImage);
+				// Add feature image
+				// Note: The feature image is always the first image in the list
+				let featureImage = document.createElement("img");
+				featureImage.src = product.images.at(0).imgSrc;
+				featureImage.alt = product.images.at(0).imgAlt;
+				productCard.appendChild(featureImage);
 
 				// Add product title
 				let productTitle = document.createElement("h3");
 				productTitle.innerText = product.title;
 				productCard.appendChild(productTitle);
-
-				if (productGroup.Name === MERCH_PRODUCT_GROUP.Name) {
-					// Add product size
-					let productSize = document.createElement("p");
-					productSize.className = "size";
-					productSize.textContent = `Size: ${product.size}`;
-					productCard.appendChild(productSize);
-				}
 
 				// Add product price
 				let productPrice = document.createElement("p");
@@ -60,35 +53,37 @@ function renderProducts(sourceFile, productGroup) {
 				productPrice.textContent = `\$${Number(product.price).toFixed(2)}`;
 				productCard.appendChild(productPrice);
 
+				// Add product description
+				let productDescription = document.createElement("p");
+				productDescription.className = "description";
+				productDescription.innerText = product.description;
+				productCard.appendChild(productDescription);
+
+				
+				// Add view details button
+				let viewDetailsBtn = document.createElement("button");
+				viewDetailsBtn.className = "btn view-details-button";
+				viewDetailsBtn.title = "View Details";
+				viewDetailsBtn.innerHTML = "View Details";
+				viewDetailsBtn.onclick = () => {
+					//TODO: open modal and display info
+					console.table(product.images)// ✅
+				};
+				productCard.appendChild(viewDetailsBtn);
+
 				// Add to cart button
 				let addToCartBtn = document.createElement("button");
-				addToCartBtn.className = "btn";
+				addToCartBtn.className = "btn add-to-cart-button";
 				addToCartBtn.title = "Add to Cart";
 				addToCartBtn.innerHTML = "Add to Cart";
 				addToCartBtn.onclick = () => {
 					// TODO: Implement add to cart functionality
-					console.log(
-						`${productGroup.Name} with id ${product.id} being added to cart`
-					);
 				};
 				productCard.appendChild(addToCartBtn);
-
-				// Add view details button
-				let viewDetailsBtn = document.createElement("button");
-				viewDetailsBtn.className = "view-details-button";
-				viewDetailsBtn.title = "View Details";
-				viewDetailsBtn.innerHTML = "View Details";
-				viewDetailsBtn.onclick = () => {
-					console.log(
-						`Showing details for ${productGroup.Name} with id ${product.id}`
-					);
-				};
-				productCard.appendChild(viewDetailsBtn);
 
 				// Add product card to the book product grid
 				productGrid.appendChild(productCard);
 
-				console.log(`${productGroup.Name} products rendered successfully`);
 			});
 		})
 		.catch((error) => {
